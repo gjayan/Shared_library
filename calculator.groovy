f019a2d2f7c786f441e1b9d64e8248e2c30acfbc
gjayan <119437626+gjayan@users.noreply.github.com>
2024-04-28 21:29:00 +0530

    Create calculator.groovy

---
+++ calculator.groovy
@@ -0,0 +1,14 @@
+def add(String x, String y){
+  int value = x.toInteger() + y.toInteger()
+  echo "Sum of ${x} and ${y} is ${value.toString()}"
+}
+def mul(x,y){
+  echo "Multiplication of ${x} and ${y} is ${x*y}"
+}
+def sub(x,y){
+  echo "Subtraction of ${x} and ${y} is ${x-y}"
+}
+def div(x,y){
+  echo "Division of ${x} and ${y} is ${x/y}"
+}
+  
