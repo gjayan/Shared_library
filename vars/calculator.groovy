8e8748da2da6087c0a3b01186cfb54c826a695d6
gjayan <119437626+gjayan@users.noreply.github.com>
2024-05-23 19:57:05 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -3,13 +3,15 @@ def add(String x, String y){
   echo "Sum of ${x} and ${y} is ${value.toString()}"
 }
 def mul(x,y){
-  echo "Multiplication of ${x} and ${y} is ${x*y}"
+  int value = x.toInteger() * y.toInteger()
+  echo "Multiplication of ${x} and ${y} is ${value}"
 }
 def sub(x,y){
   int value = x.toInteger() - y.toInteger()
   echo "Subtraction of ${x} and ${y} is ${value}"
 }
 def div(x,y){
-  echo "Division of ${x} and ${y} is ${x/y}"
+  int value = x.toInteger() / y.toInteger()
+  echo "Division of ${x} and ${y} is ${value}"
 }
   
